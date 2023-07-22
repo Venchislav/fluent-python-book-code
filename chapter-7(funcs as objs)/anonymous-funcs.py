ac7386462013dad7d6c31f835eb72c2e47a4fc0a
Venchislav <97972996+Venchislav@users.noreply.github.com>
2023-07-22 11:33:49 +0300

    Delete chapter-7(funcs as objs) directory

--- chapter-7(funcs as objs)/anonymous-funcs.py
+++
@@ -1,6 +0,0 @@
-# lambda is a keyword for anonymous function
-
-fruits = ['strawberry', 'fig', 'apple', 'cherry', 'raspberry', 'banana']
-print(sorted(fruits, key=lambda word: word[::-1]))
-
-# that's all folks!
